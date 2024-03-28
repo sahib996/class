a98943cf493ab1c23a3ddf93e69d20d102774974
sahib memmedov <sahibmemmedov7890@gmail.com>
2024-03-27 17:18:31 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Student studentOne = new Student("ibrahim", "Xushmandiyev", "AB106", 100);
+            studentOne.GetInfo();
+            studentOne.CheckGraduation();
+            studentOne.GetDiplomDegree();
+
+            Console.WriteLine();
+
+            Student studentTwo = new Student("Sahib", "Mammadov", "AB106", 0);
+            studentTwo.GetInfo();
+            studentTwo.CheckGraduation();
+            studentTwo.GetDiplomDegree();
+
+            Student studentThree = new student ("Nurlan", "Abbasov ", "AB106", 50);
+            studentThree.GetInfo();
+            studentThree.CheckGraduation();
+            studentThree.GetDiplomDegree();
+
+            Student studentFour = new Student("Rauf", "Dosduyev", "AB106", 35);
+            studentFour.GetInfo();
+            studentFour.CheckGraduation();
+            studentFour.GetDiplomDegree();
+        }
+    }
+}
---
+++ student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public int Point;
+        public bool IsGraduate;
+
+        public student( string name, string surname, string group, int point, bool isGraduate)
+        {
+            Name = name;
+            Surname = surname;  
+            Group = group;  
+            Point = point; 
+            IsGraduate = isGraduate;   
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Name; {Name}  \nSurname; {Surname}   \nGropu; {Group}   \npoint; {Point}   \nIsGraduate; {IsGraduate}");                       );
+        }
+
+        public void CheckGraduation()
+        {
+            Console.WriteLine ($"Graduation Status: {(IsGraduate ? "Bu telebe mezun olub" : "Mezun olmayib")}");
+        }
+        public void GetDiplomDegree()
+        {
+            if (Point < 65)
+            {
+                Console.WriteLine("yoxdur");
+            }
+            else if (Point >= 65 && Point < 85)
+            {
+                Console.WriteLine("adi");
+            }
+            else if (Point >= 80 && Point < 90) ;
+            {
+                Console.WriteLine("Seref");
+            }
+            else 
+            {
+                Console.WriteLine("Yuksek seref");
+            }
+        }
+    }
+}
